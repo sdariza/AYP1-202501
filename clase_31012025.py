bb6e0aba56455f13fd40228daeb951169202c290
Sebastian David Ariza Coll <39573068+sdariza@users.noreply.github.com>
2025-01-31 20:11:35 -0500

    feat: ✨add clase 31/01/2025

---
+++ clase_31012025.py
@@ -0,0 +1,48 @@
+print("Hola a todos")
+print("Este es un segundo mensaje")
+print("Este es un tercer mensaje")
+print("Mensajes con salto de linea ilustrados")
+print("Mensaje sin salto de linea", end="😒")
+print("Ver diferencia")
+
+# Comentario, línea no ejecutada
+"""
+Este script contiene el uso de la función print
+Esta es la segunda clase del curso, aboraderomos más temas
+relacionados a otras funciones
+"""
+
+print("Podemos ilustar operaciones matemáticas 🧮")
+print("+")
+print("2 + 2 =",4, "5 + 5 =",10)
+# Hay una forma más sencilla de ilutrar el mensaje anterior
+# utilizando un formato para el mensaje
+print(f"2 + 2 = {2 + 2} 5 + 5 = {5 + 5}")
+print("-")
+print(f"2 - 2 = {2 - 2}")
+print(f"5 - 20 = {5 - 20}")
+
+print("*")
+print(f"2 * 2 = {2 * 2}")
+print(f"5 * 20 = {5 * 20}")
+
+print("/")
+print(f"2 / 2 = {2 / 2}")
+print(f"5 / 20 = {5 / 20}")
+
+
+#Para conocer el tipo de dato en python, hacemos uso de la función
+# type()
+print(type(520))
+
+print(f"type(5 / 20) es {type(5 / 20)}")
+
+print("Módulo") # ¡El módulo es el residuo de la división! 😎
+print(f"56 mod 10 = {56 % 10}")
+print(f"-40 mod 11 = {-40 % 11}")
+print(f"40 mod -11 = {40 % -11}")
+
+print("División parte entera") # ¡El módulo es el residuo de la división! 😎
+print(f"56 div 10 = {56 // 10}")
+print(f"-40 div 11 = {-40 // 11}")
+print(f"40 div -11 = {40 // -11}")
